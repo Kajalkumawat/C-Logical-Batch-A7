a3ff3233a6f77cb9aeeb8f009e073910bd3e8cf2
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-27 12:45:33 +0530

    while loop basic programming questions easy to advance

---
+++ A.c
@@ -0,0 +1,125 @@
+// write a c program sum all natural number through while loop : 
+
+// natural number :1--->10 addition 
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     int sum=0;
+//     while(a<=5){
+//         sum=sum+a;
+//         a++;
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+// ------------------------------------------------------------------
+// write a c progam count digit of the number :
+
+// 6473 :4 
+
+// #include<stdio.h>
+// int main(){
+//     int a=6473;
+//     int count=0;
+//     while(a>0){
+//         a=a/10;
+//         count++;
+//     }
+//     printf("%d",count);
+//     return 0;
+// }
+// --------------------------------------------------------------
+// write a c program  sum of all the digits :
+
+// #include<stdio.h>
+// int main(){
+//    int a=1234;
+//    int rem,sum=0;
+//    while(a>0){
+//         rem=a%10;
+//         sum=sum+rem;
+//         a=a/10;
+//    }
+//    printf("%d",sum);
+//     return 0;
+// }
+// ---------------------------------------------------------
+// #include<stdio.h>
+// int main(){
+//    int a=1234;
+//    int rem,sum=0;
+//    while(a>0){
+//         rem=a%10;
+//         sum=sum+rem;
+//         a=a/10;
+//    }
+//    printf("%d",sum);
+//     return 0;
+// }
+// --------------------------------------------------------------
+// reverse number : 
+// 4328 ---->8234
+
+// #include<stdio.h>
+// int main(){
+//    int a=1234;
+//    int rem,sum=0;
+//   while(a>0){
+//       rem=a%10;
+//       sum=sum*10+rem;
+//       a=a/10;
+//   }
+//   printf("%d",sum);
+//     return 0;
+// }
+// -------------------------------------------------------------------
+// palindrome number : 
+
+// #include<stdio.h>
+// int main(){
+//    int a=12321;
+//    int temp=a;
+//    int rem,sum=0;
+//   while(a>0){
+//       rem=a%10;
+//       sum=sum*10+rem;
+//       a=a/10;
+//   }
+//   printf("%d",sum);
+//   if(sum==temp){
+//       printf("this number is pallindrome");
+//   }
+//   else{
+//       printf("this number is not pallindrome");
+//   }
+//     return 0;
+// }
+// ---------------------------------------------------------------------------
+// Armstrong number : 
+// #include<stdio.h>
+// int main(){
+//     int a=153;
+//     int temp=a;
+//     int rem,sum=0;
+//     while(a>0){
+//         rem=a%10;
+//         sum=sum+rem*rem*rem;
+//         a=a/10;
+//     }
+//     if(temp==sum){
+//         printf("this number is armstrong number ");
+//     }
+//     else{
+//         printf("this number is not armstrong ");
+//     }
+//     return 0;
+// }
+// -------------------------------------------------------------------
+
+
+
+
+
+
+
+
