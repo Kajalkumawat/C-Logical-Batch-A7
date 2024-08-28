83ab3e05167faa0313565801b244c996036b557e
Kajalkumawat <kajalkumawat0404@gmail.com>
2024-08-28 22:37:12 +0530

    basic for loop questions in C programming

---
+++ BasicForloop.c
@@ -0,0 +1,76 @@
+// #include <stdio.h>
+// int main() {
+//     int num=9;
+//     int p=num*num;
+//     int rem,sum=0;
+//     while(p>0){
+//         rem=p%10;
+//         sum=sum+rem;
+//         p=p/10;
+//     }
+//     printf("%d",sum);
+//     if(sum==num){
+        
+//     }
+//     return 0;
+// }
+// --------------------------------------------------------
+// for loop :
+// ->entry control loop 
+
+// syntax:
+// for(initialization;condition;increment/decrement){
+
+// 		//code 
+
+// }
+// --------------------------------------------------------
+// 1 2 3 4 5 
+
+// #include <stdio.h>
+// int main() {
+//     for(int i=1;i<=5;i++){
+//         printf("%d ",i);
+//     }
+//     return 0;
+// }
+// --------------------------------------------------------
+// 5 4 3 2 1 
+
+// #include <stdio.h>
+// int main() {
+//     for(int i=5;i>=1;i--){
+//         printf("%d ",i);
+//     }
+//     return 0;
+// }
+// --------------------------------------------------------
+// sum of 1 to 5 : 
+// #include <stdio.h>           
+// int main() {
+//     int sum=0;
+//     for(int i=1;i<=5;i++){
+//         sum=sum+i;
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+// --------------------------------------------------------
+// 1-->5 even number 
+
+// #include <stdio.h>
+
+// int main() {
+//     for(int i=1;i<=5;i++){
+//        if(i%2==0){
+//             printf("%d ",i);
+//        }
+//     }
+//     return 0;
+// }
+// ----------------------------------------------
+
+
+
+
+
